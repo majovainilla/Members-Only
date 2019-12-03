5c6dd88d28a13b6613e1b2f9cfc95f762214806e
majovainilla <maria_reyes@me.com>
2019-12-03 12:56:17 -0600

    Delete post validation to allow long titles

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
-  validates :title, presence: true, length: { maximum: 50 }
-  validates :body, presence: true, length: { maximum: 1000 }
+  validates :title, presence: true
+  validates :body, presence: true
 end
