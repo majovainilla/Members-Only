661b3858bcb84fcd19c99b18ce14172fad708629
Fabricio Garcia <gael.fabricio@gmail.com>
2019-12-04 10:15:14 -0600

    fix size unused parameter in user_helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
       remember @user
       flash[:success] = 'Welcome to the Members Only Club!'
       redirect_to @user
